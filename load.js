c94b08272d2e2521dd51207d109f5cfe47a30743
Alberto Harka <alb.harka@icloud.com>
2023-05-12 17:45:14 +0200

    made plugin search functional

--- load.js
+++ load.js
@@ -12,18 +12,15 @@ const __dirname = path.dirname(__filename);
 const pluginsDirPath = path.join(__dirname, PLUGINS_FOLDER);
 const pluginsDirContent = fs.readdirSync(pluginsDirPath);
 
-const plugins = pluginsDirContent.reduce((list, item) => {
-  const itemPath = path.join(pluginsDirPath, item);
-  if (!fs.lstatSync(itemPath).isDirectory()) return list;
+const plugins = pluginsDirContent
+  .map((item) => path.join(pluginsDirPath, item))
+  .filter((itemPath) => fs.lstatSync(itemPath).isDirectory())
+  .map((itemPath) => path.join(itemPath, PLUGIN_FILE))
+  .filter((pluginPath) => fs.existsSync(pluginPath));
 
-  const pluginFilePath = path.join(itemPath, PLUGIN_FILE);
-  if (fs.existsSync(pluginFilePath)) {
-    console.log(`[Found] ${pluginFilePath}`);
-    list.push(pluginFilePath);
-  }
-
-  return list;
-}, []);
+plugins.forEach((pluginPath) => {
+  console.log(`[Found] ${pluginPath}`);
+});
 
 const importPromises = plugins.map(
   (pluginPath) =>
@@ -40,7 +37,9 @@ const pluginModules = await Promise.all(importPromises);
 pluginModules.forEach(({ module, plugin: pluginPath }) => {
   if (module.default) {
     module.default(ctx);
-    console.log(`[Loaded] ${module.meta?.name ? module.meta.name : pluginPath}`);
+    console.log(
+      `[Loaded] ${module.meta?.name ? module.meta.name : pluginPath}`
+    );
   } else {
     console.log(`[Skip] ${pluginPath} CAUSE: no default export`);
   }
@@ -51,4 +50,6 @@ console.log("[App] Starting app\n");
 
 start();
 
-console.log("\n[App] Main task finished executing. Some promises could still be pending.");
+console.log(
+  "\n[App] Main task finished executing. Some promises could still be pending."
+);
